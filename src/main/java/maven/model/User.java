745f8cd869c55e83351f4b35cfd650229255e305
8xhamhoc <tomatoquang@gmail.com>
2018-04-10 08:19:45 +0700

    Create jar to run mongo

---
+++ src/main/java/maven/model/User.java
@@ -0,0 +1,48 @@
+package maven.model;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection = "user")
+public class User {
+
+    @Id
+    private String id;
+
+    private String name;
+    private Integer age;
+    private String dateOfBirth;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public String getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(String dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+}
--- src/main/java/maven/profiles/App.java
+++ src/main/java/maven/profiles/App.java
@@ -1,13 +1,33 @@
 package maven.profiles;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import maven.model.User;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.data.mongodb.core.MongoOperations;
+
+import java.util.List;
+
+public class App {
+
+    public static void main( String[] args ) {
+
+        ApplicationContext applicationContext = new ClassPathXmlApplicationContext("mongo-config.xml");
+        MongoOperations mongoOperations = applicationContext.getBean("mongoTemplate", MongoOperations.class);
+
+        /*User user = new User();
+        user.setId("12345678");
+        user.setAge(29);
+        user.setName("Quang Phan");
+        user.setDateOfBirth("1989-09-13");
+
+        mongoOperations.save(user);*/
+
+        List<User> users = mongoOperations.findAll(User.class);
+
+        for (User user : users) {
+            System.out.println("Username: " + user.getName());
+        }
+
     }
+
 }
